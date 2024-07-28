8e2fa4087e19340c8a0046ec15ffd78bd17d2072
WhamBamTurkeyAndHam <benji.olsen6@gmail.com>
2024-07-29 10:20:38 +1200

    Add round and scoring of round

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 // | Play against the computer in Rock, Paper, Scissors |
 // ------------------------------------------------------
 
+let humanScore = 0
+let computerScore = 0
+
 // Write a function that will return 'rock, 'paper', 'scissors' as the computer's move.
   // Firstly, get the computer to randomly make a move.
   // Compare the resulting number to a move using decimals between 0 and 1, such as 0 - 0.33 = 'Rock'.
@@ -9,39 +12,84 @@
 
 function getComputerChoice () {
 
-  let computerChoice = '';
+  let computerOption = '';
 
   let getRandom = Math.random();
   
   if (getRandom >= 0 && getRandom < 0.33) {
-    computerChoice = 'Rock';
+    computerOption = 'Rock';
   } else if (getRandom >= 0.34 && getRandom < 0.66) {
-    computerChoice = 'Paper';
+    computerOption = 'Paper';
   } else if (getRandom >= 0.67 && getRandom < 1) {
-    computerChoice = 'Scissors';
+    computerOption = 'Scissors';
   }
-  return computerChoice;
+  return computerOption;
 }
-
-console.log(getComputerChoice());
 
 // Write a function that will return 'rock, 'paper', 'scissors' as the human's move.
   // Firstly, get the user to input a term that is 'rock, 'paper' or 'scissors'.
-  // Allow their result to be 'Rock or 'rock'. 
+  // Allow their result to be 'Rock or 'rock'.
 
 function getHumanChoice () {
 
-  let userChoice = '';
+  let humanOption = '';
 
-  humanChoice = prompt('Pick your move.', 'Rock, Paper or Scissors')
-  if (humanChoice === 'Rock' || humanChoice === 'rock') {
-      userChoice = 'Rock';
-    } else if (humanChoice === 'Paper' || humanChoice === 'paper') {
-      userChoice = 'Paper';
-    } else if (humanChoice === 'Scissors' || humanChoice === 'scissors') {
-      userChoice = 'Scissors';
+  userChoice = prompt('Pick your move.', 'Rock, Paper or Scissors')
+  if (userChoice === 'Rock') {
+      humanOption = 'Rock';
+    } else if (userChoice === 'Paper') {
+      humanOption = 'Paper';
+    } else if (userChoice === 'Scissors') {
+      humanOption = 'Scissors';
     }
-    return userChoice;
+    return humanOption;
   }
 
-console.log(getHumanChoice());
+// Write a function that will play a single round.
+  // Take both the human choice and the computer choice as arguements.
+  // Compare the arguements.
+  // Compare if the result is a Win, Loss or a Tie.
+  // Give one (1) point to the human or computer, and logs the result.
+
+function playRound (humanChoice, computerChoice) {
+  let result = '';
+
+  if (humanChoice === 'Rock') {
+    if (computerChoice === 'Rock') {
+      result = 'You Tied.';
+    } else if (computerChoice === 'Paper') {
+      result = 'You Lose.';
+      ++computerScore
+    } else if (computerChoice === 'Scissors') {
+      result = 'You Win!';
+      ++humanScore
+    }
+  } else if (humanChoice === 'Paper') {
+    if (computerChoice === 'Rock') {
+      result = 'You Win!';
+      ++humanScore
+    } else if (computerChoice === 'Paper') {
+      result = 'You Tied.';
+    } else if (computerChoice === 'Scissors') {
+      result = 'You Lose.'
+      ++computerScore
+    }
+  } else if (humanChoice === 'Scissors') {
+    if (computerChoice === 'Rock') {
+      result = 'You Lose';
+      ++computerScore
+    } else if (computerChoice === 'Paper') {
+      result = 'You Win!';
+      ++humanScore
+    } else if (computerChoice === 'Scissors') {
+      result = 'You Tied.'
+    }
+  }
+
+  console.log(`You picked ${humanChoice}. The computer picked ${computerChoice}. ${result}`)
+}
+
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+
+playRound(humanSelection, computerSelection);
